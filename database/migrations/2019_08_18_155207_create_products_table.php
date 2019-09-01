ad3fe9e293d8fffb4045c204ec710aecd57a26cc
AlbertBuluma <albert.buluma1@gmail.com>
2019-09-01 21:10:04 +0300

    Add table fields

--- database/migrations/2019_08_18_155207_create_products_table.php
+++ database/migrations/2019_08_18_155207_create_products_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\Product;
+
 
 class CreateProductsTable extends Migration
 {
@@ -15,6 +17,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('description',1000);
+            $table->integer('quantity')->unsigned();
+            $table->string('status')->default(Product::UNAVAILABLE_PRODUCT);
             $table->timestamps();
         });
     }
