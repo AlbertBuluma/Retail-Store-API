f2d91b5594ee59784bfbbe19d5dfc4a37f54396b
AlbertBuluma <albert.buluma1@gmail.com>
2019-09-01 21:32:02 +0300

    Configure Product and Category model factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -31,3 +31,23 @@ $factory->define(User::class, function (Faker $faker) {
         'admin' => $verified = $faker->randomElement([User::ADMIN_USER, User::REGULAR_USER]),
     ];
 });
+
+
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+    ];
+});
+
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1, 10),
+        'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT]),
+        'image' => $faker->randomElement(['1.jpg', '2.jpg', '3.jpg']),
+        'seller_id' => User::all()->random()->id,
+    ];
+});
