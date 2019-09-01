dae6d35642970fe478487bce4b88be5fe13e18da
AlbertBuluma <albert.buluma1@gmail.com>
2019-09-01 21:31:40 +0300

    Configure user model factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,9 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Category;
+use App\Product;
 use App\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
@@ -23,5 +26,8 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'verified' => $verified = $faker->randomElement([User::VERIFIED_USER, User::UNVERIFIED_USER]),
+        'verification_token' => $verified == User::VERIFIED_USER ? null : User::generateVerificationCode(),
+        'admin' => $verified = $faker->randomElement([User::ADMIN_USER, User::REGULAR_USER]),
     ];
 });
