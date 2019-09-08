d590e07f686661b4c907a6ae4c19f46234e56568
AlbertBuluma <albert.buluma1@gmail.com>
2019-09-08 10:58:50 +0300

    Return updated user's json response

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -125,6 +125,8 @@ class UserController extends Controller
             $user->admin = $request->admin;
 
             $user->save();
+
+            return response()->json(['data' => $user], 200);
         }
     }
 
